ae8cf091091e42ddb97fbb49e8835311bd6503a2
onalennaleso <158218748+onalennaleso@users.noreply.github.com>
2024-07-25 09:02:39 -0600

    removal of files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Onalenna Leso and is {" "}
+        <a href="https://github.com/onalennaleso/weather-react-app" target="_blank">
+        open-sourced on GitHub
         </a>
-      </header>
+      </footer>
+    
     </div>
   );
 }
 
-export default App;
+
