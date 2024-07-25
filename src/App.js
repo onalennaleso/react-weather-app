932813e978040b8f6748852e0cf5eaa246089c18
onalennaleso <158218748+onalennaleso@users.noreply.github.com>
2024-07-25 10:08:28 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Onalenna Leso and is {" "}
         <a href="https://github.com/onalennaleso/react-weather-app" target="_blank">
         open-sourced on GitHub
         </a>
-      </footer>
-    
+        </footer>
+        </div>
     </div>
   );
 }
