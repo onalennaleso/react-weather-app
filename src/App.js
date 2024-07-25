52b2e8113ea1f0649799779031fc04687dec711f
onalennaleso <158218748+onalennaleso@users.noreply.github.com>
2024-07-25 09:16:32 -0600

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by Onalenna Leso and is {" "}
-        <a href="https://github.com/onalennaleso/weather-react-app" target="_blank">
+        <a href="https://github.com/onalennaleso/react-weather-app" target="_blank">
         open-sourced on GitHub
         </a>
       </footer>
